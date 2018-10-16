174a34e724b8f8437d8c9cb259060bcc11752eef
liyan <yanx0167@stud.kea.dk>
2018-10-16 21:16:52 +0200

    added: for each block of 9 digits

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 "use strict";
-let randomColumn = Math.ceil(Math.random() * 3);
-let randomRow = Math.ceil(Math.random() * 3);
-console.log(randomColumn, randomRow);
 
-const cellIndex = Math.ceil(Math.random() * 9);
-console.log(cellIndex);
-const cell = document.querySelector(`option:nth-of-type(${cellIndex})`);
-cell.setAttribute("selected", "");
-cell.parentElement.style.gridColumnStart = randomColumn; // option is inside select, select is grid element
-cell.parentElement.style.gridRowStart = randomRow;
-console.log(cell);
+const sectionS = document.querySelectorAll("section");
+sectionS.forEach(buildResult);
+function buildResult(s) {
+  let randomColumn = Math.ceil(Math.random() * 3);
+  let randomRow = Math.ceil(Math.random() * 3);
+  console.log(randomColumn, randomRow);
+
+  const cellIndex = Math.ceil(Math.random() * 9);
+  console.log(cellIndex);
+  const cell = s.querySelector(`option:nth-of-type(${cellIndex})`);
+  cell.setAttribute("selected", "");
+  cell.parentElement.style.gridColumnStart = randomColumn; // option is inside select, select is grid element
+  cell.parentElement.style.gridRowStart = randomRow;
+  console.log(cell);
+}
