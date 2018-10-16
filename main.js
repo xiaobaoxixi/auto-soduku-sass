b4728339cf94e0266f21e5dc60641fbf04033a52
liyan <yanx0167@stud.kea.dk>
2018-10-16 22:28:15 +0200

    added: set data-col and data-row to each digit, prepare for later validation

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 
 const sectionS = document.querySelectorAll("section");
 sectionS.forEach(buildResult);
-function buildResult(s) {
+function buildResult(s, i) {
   const allFieldS = s.querySelectorAll("select");
   let position = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   allFieldS.forEach(arrangeDigitsInEach3by3);
   function arrangeDigitsInEach3by3(sel, selI) {
+    const selCol = (i % 3) * 3 + 1 + (selI % 3);
+    const selRow = (selI - (selI % 3)) / 3 + (i - (i % 3));
+    sel.setAttribute("data-row", selRow);
+    sel.setAttribute("data-col", selCol);
+
     sel
       .querySelector(`option:nth-of-type(${selI + 1})`)
       .setAttribute("selected", ""); // by default, assign digit value based on their index+1, so that digits range from 1-9
