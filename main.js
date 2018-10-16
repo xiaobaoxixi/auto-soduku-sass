32f4acca825f88f67bf4d0208c45df21962e77af
liyan <yanx0167@stud.kea.dk>
2018-10-16 21:55:29 +0200

    random put 1-9 into each 3x3 field

--- main.js
+++ main.js
@@ -3,15 +3,22 @@
 const sectionS = document.querySelectorAll("section");
 sectionS.forEach(buildResult);
 function buildResult(s) {
-  let randomColumn = Math.ceil(Math.random() * 3);
-  let randomRow = Math.ceil(Math.random() * 3);
-  console.log(randomColumn, randomRow);
-
-  const cellIndex = Math.ceil(Math.random() * 9);
-  console.log(cellIndex);
-  const cell = s.querySelector(`option:nth-of-type(${cellIndex})`);
-  cell.setAttribute("selected", "");
-  cell.parentElement.style.gridColumnStart = randomColumn; // option is inside select, select is grid element
-  cell.parentElement.style.gridRowStart = randomRow;
-  console.log(cell);
+  const allFieldS = s.querySelectorAll("select");
+  let position = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  allFieldS.forEach(arrangeDigitsInEach3by3);
+  function arrangeDigitsInEach3by3(sel, selI) {
+    sel
+      .querySelector(`option:nth-of-type(${selI + 1})`)
+      .setAttribute("selected", ""); // by default, assign digit value based on their index+1, so that digits range from 1-9
+    const cellIndex = position[Math.floor(Math.random() * position.length)];
+    const columnStart = cellIndex % 3 === 0 ? 3 : cellIndex % 3;
+    const rowStart = (cellIndex - columnStart) / 3 + 1;
+    sel.querySelector(
+      `option:nth-of-type(${cellIndex})`
+    ).parentElement.style.gridColumnStart = columnStart;
+    sel.querySelector(
+      `option:nth-of-type(${cellIndex})`
+    ).parentElement.style.gridRowStart = rowStart; // rearrange the digit based on random pick from position array
+    position.splice(position.indexOf(cellIndex), 1); // update position array, so each position (1-9) only gets picked once
+  }
 }
