523e114955a3e7cf8c2f0cfcc5d05fcacd10c4d9
liyan <yanx0167@stud.kea.dk>
2018-10-16 20:48:14 +0200

    grid setup

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+// let randomColumn = Math.ceil(Math.random() * 3);
+// let randomRow = Math.ceil(Math.random() * 3);
+// console.log(randomColumn, randomRow);
+
+// const inputIndex = Math.ceil(Math.random() * 9);
+// const input = document.querySelector(`input:nth-of-type(${inputIndex})`);
+// input.style.gridColumnStart = randomColumn;
+// input.style.gridRowStart = randomRow;
+// console.log(input);
