3c0be86525de8e39a42553e5cabf9e3087cc7062
liyan <yanx0167@stud.kea.dk>
2018-10-17 06:15:58 +0200

    added: target numbers in each row

--- main.js
+++ main.js
@@ -27,3 +27,27 @@ function buildResult(s, i) {
     position.splice(position.indexOf(cellIndex), 1); // update position array, so each position (1-9) only gets picked once
   }
 }
+check();
+function check() {
+  for (let rowNr = 1; rowNr < 10; rowNr++) {
+    let fieldRow;
+    if (rowNr >= 1 && rowNr <= 3) {
+      fieldRow = 1;
+    } else if (rowNr >= 4 && rowNr <= 6) {
+      fieldRow = 2;
+    } else if (rowNr >= 7 && rowNr <= 9) {
+      fieldRow = 3;
+    }
+    const row = document.querySelectorAll(
+      `section:nth-of-type(3n+${fieldRow}) [style^='grid-column-start: ${
+        rowNr % 3 === 0 ? 3 : rowNr % 3
+      }']`
+    );
+    console.log(row);
+    let valueS = [];
+    row.forEach(r => {
+      valueS.push(r.querySelector("[selected]").getAttribute("value"));
+    });
+    console.log(valueS);
+  }
+}
